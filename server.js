82c6019e27e6e6e8ce0bae4da5694e0c2ec1a71b
ronyahmmod <rony.jib@gmail.com>
2021-02-03 07:33:41 +0600

    Complete server configuration, installing needed packages and start building api

---
+++ server.js
@@ -0,0 +1,44 @@
+
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+    console.log('Uncaught exception: 😥😥😥. Server is shutting down');
+    console.log(`Error: ${err.name} and Info: ${err.message}`)
+    process.exit(1);
+})
+
+const app = require('./app');
+
+dotenv.config({
+    path: './config.env'
+});
+
+const port = process.env.APP_PORT;
+const DB = process.env.DB_LINK;
+
+// connecting with mongodb database
+mongoose.connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(con => {
+    console.log('Database connection is successfull 😀');
+})
+
+const server = app.listen(port, () => {
+    console.log(`App is running on 😀 http://localhost:${port}`)
+});
+
+process.on('unhandledRejection', err => {
+    
+    console.log('Unhandled promise rejection 😥. Server is shutting down imiditely.');
+    console.log(`Error: ${err.name} and Info: ${err.message}`);
+    server.close(() => {
+        process.exit(1);
+    })
+})
+
+
+

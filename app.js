ade3954d76b58d2bac5e9a3e879da8178a1533ce
ronyahmmod <rony.jib@gmail.com>
2021-02-05 04:47:29 +0600

    Setup express app and add some packages

--- app.js
+++ app.js
@@ -1,8 +1,79 @@
+const path = require('path');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cors = require('cors');
 const express = require('express');
+
 const AppError = require('./utils/appError');
 const catchAsync = require('./utils/catchAsync');
+const globalErrorHandler = require('./controllers/errorController');
 
+// Start express app
 const app = express();
+
+app.enable('trust proxy');
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Global middleware
+// Implement CORS
+app.use(cors());
+app.options('*', cors());
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set security http headers
+app.use(helmet());
+
+// Development loging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [],
+  })
+);
+
+app.use(compression());
+
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.cookies);
+  next();
+});
+app.use(globalErrorHandler);
 
 // alias all routes. If there is no match on any routes
 app.all('*', (req, res, next) => {
